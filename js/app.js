859b9bdb35db93fc81a8067edd602737cfe25be5
Arun Sahadeo <arunjamessahadeo@gmail.com>
2017-06-15 21:12:21 +0100

    Add keycode exceptions

--- js/app.js
+++ js/app.js
@@ -6,6 +6,26 @@ const withCaret = (value) => `${value}<span class="caret"></span>`;
 let command = '';
 document.onkeydown = (evt) => {
   // Handle any shortcuts you want
+  /* Alt key */
+  if (evt.keyCode === 18 || evt.key === 18) {
+   	return false;
+  }
+  /* Shift key */
+  if (evt.keyCode === 16 || evt.key === 16) {
+  	return false;
+  }
+  /* F5 */
+  if (evt.keyCode === 116 || evt.key === 116) {
+  	return false;
+  }
+  /* Ctrl key */
+  if (evt.keyCode === 17 || evt.key === 17) {
+  	return false;
+  }
+  /* TAB */
+  if (evt.keyCode === 9 || evt.key === 9) {
+  	return false;
+  }
   switch (evt.key) {
   	case 'Enter':
   		console.log(command);
